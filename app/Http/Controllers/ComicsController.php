4dec59d5a368d558ef783e886fde2fab550bb529
gatsuBerserk <berserkdev@outlook.it>
2022-05-17 17:45:23 +0200

    index page comics

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Comic;
 use Illuminate\Http\Request;
 
 class ComicsController extends Controller
 {
     public function index(){
-        return view("comics.index");
+        $comics = Comic::all();
+        return view("comics.index", compact("comics") );
     }
 }
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,6 +2,30 @@
 @section('title', "Laravel CRUD")
 
 @section('main')
-    
+<div class="container-fluid"> 
+    <div class="row">
+        @foreach ($comics as $comic)
+        <div class="col-3">
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                  <div class="col-md-4">
+                    <img src="{{$comic->thumb}}" class="img-fluid rounded-start" alt="{{$comic->title}}">
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title">{{$comic->title}}</h5>
+                      <p class="card-text">{{$comic->series}}</p>
+                      <p class="card-text">{{$comic->sale_date}}</p>
+                      <p class="card-text">{{ucfirst($comic->type)}}</p>
+                      <p class="card-text">{{$comic->price}} €</p> 
+                      <a href="#"><button type="button" class="btn btn-dark">Info</button></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        </div>
+        @endforeach
+    </div>
+</div>
 
 @endsection
